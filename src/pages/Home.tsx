37e2c1c5ce607f77ef116f08d2a1f33a91984710
vamsigugulothu <vamsinayak18@gmail.com>
2024-06-27 23:19:32 +0530

    initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from '../components/Head';
+import Body from '../components/Body';
+
+const Home = () => {
+
+    return (
+        <div>
+            <Head/>
+            <Body/>
+        </div>
+    );
+};
+
+export default Home;
