323088f99c9bb895f91aee83243ba3b3b98f16b1
Gugulothu Vamsi <87639722+vamsigugulothu@users.noreply.github.com>
2024-07-09 17:30:25 +0530

    Update ApiContants.js

--- src/constants/ApiContants.js
+++ src/constants/ApiContants.js
@@ -3,4 +3,5 @@ export const URL = {
   popularVideos: `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=${API_KEY}&maxResults=50`,
   videoDetails: `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=${API_KEY}`,
   searchSuggestions: `https://suggestqueries-clients6.youtube.com/complete/search?client=youtube&q=jj`,
+  searchVideo: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&key=${API_KEY}&q=`
 };
